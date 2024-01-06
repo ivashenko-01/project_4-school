147165555a5347d5858dd454183b486fe849f07d
ivashenko-01 <ivashenko-01@mail.ru>
2024-01-06 23:25:04 +0300

    5 часов работы

---
+++ assets/scripts/javascript/main.js
@@ -0,0 +1,43 @@
+const swiperAudience = new Swiper('.swiper-audience', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    
+    grabCursor: true,
+  
+    // centeredSlides: 'auto',
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+  
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: true,
+    },
+  
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //   },
+  
+  });
+
+const swiperVariation = new Swiper('.swiper-variation', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    
+    grabCursor: true,
+  
+    // centeredSlides: 'auto',
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+  
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: true,
+    },
+  
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //   },
+  
+  });
