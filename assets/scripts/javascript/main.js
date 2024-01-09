ca79774051d0c0716431a7683c90b9da882d55f4
ivashenko-01 <ivashenko-01@mail.ru>
2024-01-10 00:39:20 +0300

    modified:   assets/scripts/javascript/main.js 	modified:   assets/styles/css/media.css 	modified:   assets/styles/css/styles.css 	modified:   index.html

--- assets/scripts/javascript/main.js
+++ assets/scripts/javascript/main.js
@@ -1,35 +1,36 @@
-// const swiperAudience = new Swiper('.swiper-audience', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
+const swiperAudience = new Swiper('.swiper-audience', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
     
-//     grabCursor: true,
+    grabCursor: true,
   
-//     // centeredSlides: 'auto',
-//     slidesPerView: 'auto',
-//     spaceBetween: 30,
+    // centeredSlides: 'auto',
+    slidesPerView: 'auto',
+    // spaceBetween: 30,
   
-//     autoplay: {
-//         delay: 6000,
-//         disableOnInteraction: true,
-//     },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: true,
+    },
   
-//     // pagination: {
-//     //     el: '.swiper-pagination',
-//     //   },
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //   },
   
-//   });
+  });
 
 // const swiperVariation = new Swiper('.swiper-variation', {
 //     // Optional parameters
+    
 //     direction: 'horizontal',
 //     loop: true,
     
 //     grabCursor: true,
   
 //     // centeredSlides: 'auto',
-//     slidesPerView: 'auto',
-//     spaceBetween: 30,
+//     slidesPerView: 1.5,
+//     // spaceBetween: 30,
   
 //     autoplay: {
 //         delay: 3000,
@@ -39,5 +40,21 @@
 //     // pagination: {
 //     //     el: '.swiper-pagination',
 //     //   },
-  
+
+//     breakpoints: {
+
+//         320: {
+//             slidesPerView: 1,
+//         },
+
+//         600: {
+//             slidesPerView: 3,
+//         },
+
+//         1024: {
+//             slidesPerView: 3,
+//         },
+
+//       }
+
 //   });
